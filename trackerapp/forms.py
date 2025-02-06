36d2b4725e8100bdabd1e9f582428e8d9878b156
manishalama123 <manishalama136@gmail.com>
2025-02-06 17:46:17 +0545

    bootstrap and django form syling

--- trackerapp/forms.py
+++ trackerapp/forms.py
@@ -15,3 +15,9 @@ class TrackerForm(forms.ModelForm):
     class Meta:
         model = Tracker
         fields = ['amount', 'category', 'description', 'receipts']
+        widgets = {
+            'amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
